c449ac959c780958eb39c7c9b472cfafa9194de6
solarnius <solarnius@sagods.com>
2024-03-17 23:02:24 -0700

    show SOL roi as comparison

--- app/ROI.tsx
+++ app/ROI.tsx
@@ -3,72 +3,80 @@
 import { useEffect, useState } from "react";
 import portfolio from "./portfolio.json";
 
-async function getPositions() {
-  const tokenAccounts = await fetch(
-    "https://donnie-bx0ny6-fast-mainnet.helius-rpc.com/",
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        jsonrpc: "2.0",
-        id: 1,
-        method: "getTokenAccountsByOwner",
-        params: [
-          "D265nxGn6e62VC9oJ2gVjr2rcRTeTqwhPAqKZmXNBiYc",
-          { programId: "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA" },
-          {
-            encoding: "jsonParsed",
-          },
-        ],
-      }),
-    }
-  );
-  const tokenAccountsJson = await tokenAccounts.json();
-
-  let wallet = {} as { [key: string]: number };
-
-  for (let i = 0; i < tokenAccountsJson.result.value.length; i++) {
-    const tokenAccount = tokenAccountsJson.result.value[i];
-    const balance = tokenAccount.account.data.parsed.info.tokenAmount.uiAmount;
-    const mint = tokenAccount.account.data.parsed.info.mint;
-
-    if (balance > 0) {
-      wallet[mint] = balance;
-    }
-  }
-
-  const jupiterUrl = `https://price.jup.ag/v4/price?ids=${Object.keys(
-    wallet
-  ).join(",")}`;
-
-  const jupiterResponse = await fetch(jupiterUrl);
-  const jupiterJson = await jupiterResponse.json();
-
-  let walletBalance = 0;
-
-  const tokens = Object.keys(jupiterJson.data);
-  let values = {} as { [key: string]: number };
-
-  for (let i = 0; i < tokens.length; i++) {
-    const token = tokens[i];
-    const balance = wallet[token];
-    const price = jupiterJson.data[token].price;
-    const mintSymbol = jupiterJson.data[token].mintSymbol;
-
-    const value = balance * price;
-
-    walletBalance += value;
-    values[mintSymbol] = value;
-  }
-
-  return values;
-}
-
 export default function ROI() {
+  const [solPrice, setSolPrice] = useState(0);
   const [balance, setBalance] = useState(0);
   const [positions, setPositions] = useState({} as { [key: string]: number });
+
+  async function getPositions() {
+    const tokenAccounts = await fetch(
+      "https://donnie-bx0ny6-fast-mainnet.helius-rpc.com/",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          jsonrpc: "2.0",
+          id: 1,
+          method: "getTokenAccountsByOwner",
+          params: [
+            "D265nxGn6e62VC9oJ2gVjr2rcRTeTqwhPAqKZmXNBiYc",
+            { programId: "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA" },
+            {
+              encoding: "jsonParsed",
+            },
+          ],
+        }),
+      }
+    );
+    const tokenAccountsJson = await tokenAccounts.json();
+
+    let wallet = {} as { [key: string]: number };
+
+    for (let i = 0; i < tokenAccountsJson.result.value.length; i++) {
+      const tokenAccount = tokenAccountsJson.result.value[i];
+      const balance =
+        tokenAccount.account.data.parsed.info.tokenAmount.uiAmount;
+      const mint = tokenAccount.account.data.parsed.info.mint;
+
+      if (balance > 0) {
+        wallet[mint] = balance;
+      }
+    }
+
+    const jupiterUrl = `https://price.jup.ag/v4/price?ids=So11111111111111111111111111111111111111112,${Object.keys(
+      wallet
+    ).join(",")}`;
+
+    const jupiterResponse = await fetch(jupiterUrl);
+    const jupiterJson = await jupiterResponse.json();
+
+    setSolPrice(
+      jupiterJson.data["So11111111111111111111111111111111111111112"].price ?? 0
+    );
+
+    let walletBalance = 0;
+
+    const tokens = Object.keys(jupiterJson.data).filter(
+      (t) => t !== "So11111111111111111111111111111111111111112"
+    );
+    let values = {} as { [key: string]: number };
+
+    for (let i = 0; i < tokens.length; i++) {
+      const token = tokens[i];
+      const balance = wallet[token];
+      const price = jupiterJson.data[token].price;
+      const mintSymbol = jupiterJson.data[token].mintSymbol;
+
+      const value = balance * price;
+
+      walletBalance += value;
+      values[mintSymbol] = value;
+    }
+
+    return values;
+  }
 
   useEffect(() => {
     getPositions().then((wallet) => {
@@ -90,11 +98,23 @@ export default function ROI() {
         className="text-lg"
         style={{ color: balance > 1000 ? "green" : "red" }}
       >
+        Meme ROI:{" "}
         {(balance - 1000).toLocaleString("en-US", {
           style: "currency",
           currency: "USD",
         })}{" "}
         ({(((balance - 1000) / 1000) * 100).toFixed(2)}%)
+      </h2>
+      <h2
+        className="text-lg"
+        style={{ color: solPrice > 179.5218 ? "green" : "red" }}
+      >
+        SOL ROI:{" "}
+        {(solPrice * (1000 / 179.5218) - 1000).toLocaleString("en-US", {
+          style: "currency",
+          currency: "USD",
+        })}{" "}
+        ({(100 * (solPrice / 179.5218 - 1)).toFixed(2)}%)
       </h2>
       <table className="m-8 text-xs">
         <tr>
